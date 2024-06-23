9d4e2679e69fa5c2a3ec919fdc53bacc6227e83a
saileshghimire <saileshghimire1@gmail.com>
2024-06-23 23:22:32 +0545

    shortcut url

--- backend/src/routes/hittingurl.ts
+++ backend/src/routes/hittingurl.ts
@@ -1,9 +1,28 @@
-import { Router } from "express";
+import { Router,Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const hitting:Router = Router();
+const prisma = new PrismaClient()
 
-hitting.get('/:shortId', (req,res)=>{
-    
+hitting.get('/:shortId', async (req:Request,res:Response)=>{
+    const shortId = req.params;
+    try{
+        const url = await prisma.table.findFirst({
+            where:{
+                shortId:shortId
+            },
+            select:{
+                originalUrl:true
+            }
+        });
+        if(!url){
+            return res.status(404).json({ error: 'Short URL not found' });
+        }
+        return res.redirect(url);
+    }catch(error){
+        console.error(error);
+    return res.status(500).json({ error: 'Failed to redirect to original URL' });
+    }
 });
 
 export default hitting;
